0de93294a22032e7cbe65cb949a353c6177880cd
mijelblack677-ctrl <mijelblack677@gmail.com>
2025-10-25 11:07:14 +0200

    Create check-session.js

---
+++ pages/auth/check-session.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+export default function CheckSession() {
+  useEffect(() => {
+    const userSession = localStorage.getItem('user_session');
+    
+    if (userSession) {
+      // User is already verified, redirect to YouTube
+      window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=share&artist_welcome=true';
+    } else {
+      // No session found, redirect to Facebook login
+      window.location.href = '/auth/facebook';
+    }
+  }, []);
+
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      background: '#f0f2f5',
+      fontFamily: 'Helvetica, Arial, sans-serif'
+    }}>
+      <div>Checking your session...</div>
+    </div>
+  );
+}
